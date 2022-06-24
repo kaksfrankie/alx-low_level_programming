e9dc9ac953a2af6b3bfbac886af08fe51135f7b4
kaksfrankie <kaksfrankie@hotmail.com>
2022-06-24 08:27:19 -0700

    Modify

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /*
  * Main-Entry
  *
- * Return: 0 (Success)
+ * Return: 1
  */int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	return (0);
+	return (1);
 }
