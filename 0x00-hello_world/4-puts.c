c8c0ec7c252243a70b06437d8db096cb2304f04a
kaksfrankie <kaksfrankie@hotmail.com>
2022-06-24 04:48:59 -0700

    Modify

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-int main(void)
+/*
+ * Write a C program that prints exactly
+ * "Programming is like building a multilingual puzzle, followed by a new line.
+ */
+
+int main (void)
 {
-	char myStr[] = "Programming is like building a multilingual puzzle";
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(myStr);
 
-	return 0;
+	return(0);
 
 }
 
