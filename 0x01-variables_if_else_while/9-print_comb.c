c43a7241f315f0fdbcdb4bb19576b7ef1ad2ef17
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-01 10:36:28 +0000

    Modify

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,8 +8,11 @@ int main(void)
 	 for(x = '0'; x <= '9'; x++)
 	 {
 		 putchar(x);
-		 putchar(',');
-		 putchar(' ');
+		 if(x != '9')
+		 {
+			 putchar(',');
+			 putchar(' ');
+		 }
 	 }
 	 putchar('\n');
 	 return (0);
