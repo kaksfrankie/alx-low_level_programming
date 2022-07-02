c589798dc1d1a78bf7d09755dca53526dee033f7
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-02 11:59:13 +0000

    3Digit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+*Betty style code for function main
+*/int main(void)
+{
+	int x, y, z;
+
+	for (x = '0'; x <= '7'; x++)
+	{
+		for (y = x + 1; y <= '8'; y++)
+		{
+			for (z = y + 1; z <= '9'; z++)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(z);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
