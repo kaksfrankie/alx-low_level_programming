964485171e26c310429a43ca441f4577d9de970a
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-03 14:19:11 +0000

    Modify

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
-/*betty sy\tyle code for function main */
-int main(void)
+/*
+*betty sy\tyle code for function main
+*/int main(void)
 {
 	char x;
-	for(x = 'z'; x >= 'a'; x--)
+
+	for (x = 'z'; x >= 'a'; x--)
 	{
 		putchar(x);
 	}
