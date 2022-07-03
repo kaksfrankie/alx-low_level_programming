fc3ba11fc4dc0e2910ef36827fe3e477c9e13d33
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-03 14:04:20 +0000

    Modify

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/*betty style doc for function main */
-int main(void)
+/*
+*betty style doc for function main
+*/int main(void)
 {
 	char x;
 
-	for(x = 'a'; x <= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		putchar(x);
 	}
-	for(x = 'A'; x <= 'Z'; x++)
+	for (x = 'A'; x <= 'Z'; x++)
 	{
 		putchar(x);
 	}
-
+	putchar('\n');
 	return (0);
 }
