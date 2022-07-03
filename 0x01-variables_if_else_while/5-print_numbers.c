d1cf15188b3f6e10676ea86e60ab419cd99b1928
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-03 14:14:33 +0000

    Modify

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-
-/*betty style code for function main */
-int main(void)
+/*
+*betty style code for function main
+*
+*/int main(void)
 {
 	int x;
-	for(x = 0; x <= 9; x++)
+
+	for (x = 0; x <= 9; x++)
 	{
-		printf("%d\n", x );
+		printf("%d", x);
 	}
+	putchar('\n');
 	return (0);
 }
-
