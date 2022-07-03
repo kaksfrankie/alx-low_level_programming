a1f4cd8d73a3218cb6ac00654a5f9b9c964a98f4
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-03 13:59:17 +0000

    Modify

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/*betty doc for function main */
-int main(void)
+/*
+*betty doc for function main
+*/int main(void)
 {
 	char x;
 
-	for(x = 'a'; x<= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		putchar(x);
+		putchar('\n');
 	}
 	return (0);
 }
