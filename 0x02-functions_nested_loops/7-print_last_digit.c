9cd08158dc680495e12bac5993dde3b48fed92da
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-05 18:34:40 +0000

    Modify

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,28 @@
 #include "main.h"
+int pos(int x);
 /*
 *return last digit
 */int print_last_digit(int a)
 {
-	int x;
+	int y;
 
-	x = a % 10;
+	if (a < 0)
+	{
+		a = pos(a);
+	}
+	else
+	{
+		a = a;
+	}
+	y = a % 10;
+	_putchar(y);
 	return (x);
-	_putchar(x);
 	_putchar('\n');
+}
+/*
+*get the absolute value
+*/int pos(int x)
+{
+	x = x * (-1);
+	return (x);
 }
