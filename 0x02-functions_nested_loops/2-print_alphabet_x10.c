8d5c55a0c6581a4b47d53f108ead3d7dacdc293a
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-05 16:27:13 +0000

    Add

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+*print 10 rows of the alphabet
+*/void print_alphabet_x10(void)
+{
+	int i;
+	char x;
+	for (i = 0; i <= 9; i++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
