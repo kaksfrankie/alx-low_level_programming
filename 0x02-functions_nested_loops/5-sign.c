f22b68a280406293298d619ef629cf96eee411bf
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-05 17:14:57 +0000

    modify

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,19 +6,16 @@
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
 		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		_putchar(',');
 		return (-1);
 	}
 	else
 	{
-		_putchar(0);
-		_putchar(',');
+		_putchar('0');
 		return (0);
 	}
 	_putchar('\n');
