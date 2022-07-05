3bd7189db36cfb07caa40471c5e9c36b859bfc94
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-05 17:11:59 +0000

    Add

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.c"
+/*
+*prints the number sign
+*/int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchar(',');
+		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		_putchar(',');
+		return (0);
+	}
+	_putchar('\n');
+}
