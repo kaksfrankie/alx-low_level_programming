62479b6078344a050967ab614acfbe6ad6572303
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-06 08:26:03 +0000

    Modify

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 0)
+	/*while (i < 0)
 	{
-		putchar(i); /* i should be incremented after this*/
-	}
+		putchar(i);
+	}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
