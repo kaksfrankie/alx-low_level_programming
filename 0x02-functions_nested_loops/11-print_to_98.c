718a0c93abf730eef79a1c144d61aeae90facc3b
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-06 02:01:07 +0000

    Modify

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/*
+*print from 0 to 98
+*/int print_to_98(int n)
+{
+	int counter;
+
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d\n", n);
+	}
+}
