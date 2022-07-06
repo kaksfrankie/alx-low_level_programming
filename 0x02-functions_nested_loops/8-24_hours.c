32075206d55c53f00701e165d43de820609a5a36
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-06 00:19:58 +0000

    modify

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,13 +5,13 @@
 {
 	int i, j, x, y;
 
-	for (i = 0; i <= 2; i++)
+	for (i = '0'; i <= '2'; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (j = '0'; j <= '3'; j++)
 		{
-			for (x = 0; x <= 5; x++)
+			for (x = '0'; x <= '5'; x++)
 			{
-				for (y = 0; y <= 9; y++)
+				for (y = '0'; y <= '9'; y++)
 				{
 					_putchar(i);
 					_putchar(j);
