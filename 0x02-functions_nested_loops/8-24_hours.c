09cbcd89a8e35a919456daf062827923fb747236
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-06 00:32:50 +0000

    Modify

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,24 +3,18 @@
 *prints every minute of the day
 */ void jack_bauer(void)
 {
-	int i, j, x, y;
+	int i, j;
 
-	for (i = '0'; i <= '2'; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		for (j = '0'; j <= '3'; j++)
+		for (j = 0; j <= 59; j++)
 		{
-			for (x = '0'; x <= '5'; x++)
-			{
-				for (y = '0'; y <= '9'; y++)
-				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(':');
-					_putchar(x);
-					_putchar(y);
-					_putchar('\n');
-				}
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
