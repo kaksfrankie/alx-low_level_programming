6612b84cd8486da1229410d541d8271ed317f770
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-07 12:34:17 +0000

    Modify

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	while ((i != 2) && (i != 4))
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i + '0');
+		while ((i != 2) && (i != 4))
+		{
+			_putchar(i + '0');
+		}
 	}
 
 	_putchar('\n');
