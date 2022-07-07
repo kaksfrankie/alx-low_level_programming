e285e56c19c620013e9429ddb9b375eb5bb9c7ed
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-07 14:28:01 +0000

    Modify

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,14 +11,14 @@ void print_line(int n)
 {
 	int i;
 
-	while (n <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 
 	else
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 1; i < n; i++)
 		{
 			_putchar(95);
 		}
