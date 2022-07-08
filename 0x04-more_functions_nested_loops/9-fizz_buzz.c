aa7444ccfbf9f1570ec01050a16fa6fb8c09f414
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-08 02:14:08 +0000

    Modify

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,24 +12,26 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("Fizz ");
+			printf("FizzBuzz");
 		}
-
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
-
-		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+		}
+
+		if (i != 100)
+		{
+			putchar(' ');
 		}
 	}
 
