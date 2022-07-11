c542e8f9a8343b6e3e6c38505401884492e1f83f
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-11 15:57:55 +0000

    lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_strlen - returns the lenth of the string
+*@s:string 
+*
+*Return:lenth
+*/
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
