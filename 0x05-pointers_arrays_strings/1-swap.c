d12b142ff92af1015bc89e181a27e69faea83ffb
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-11 15:47:50 +0000

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*swap_int - takes two values and swaps them
+*@a:first value
+*@b:second value
+*
+*Return: none
+*/
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tem = *a;
+	*a = *b;
+	*b = tem;
+}
