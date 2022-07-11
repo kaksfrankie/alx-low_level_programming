ddf6bbc9d91af407cdac4b60914dd1f6b032ac90
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-11 16:07:29 +0000

    Modify

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_puts - prints a string followed by a new line
+*@str: string to be printed
+*
+*return: none
+*/
+
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; *str != '\0'; s++)
+	{
+		putchar(*str);
+	}
+}
