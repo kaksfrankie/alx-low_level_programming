4ac9296fa130b85ec3bb8b81651cd3d550e19e96
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-12 08:30:30 +0000

    Add

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_strcpy - copies a string.
+*@dest: destination
+*@src: source
+*
+*Return : char
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < i; j++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (*dest);
+}
