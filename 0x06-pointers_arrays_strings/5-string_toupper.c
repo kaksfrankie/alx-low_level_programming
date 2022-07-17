d61bdbed8df8a55817eaccdfc852f01517942225
kaksfrankie <kaksfrankie@gotmail.com>
2022-07-17 17:51:53 +0000

    Add

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*string_toupper - changes all lower case to upper case
+*@s: string to be changed
+*
+*Return: char
+*/
+
+char *string_toupper(char *s)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+
+		a++
+	}
+
+	return (p);
+}
